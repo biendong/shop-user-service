0635731b25439c91370c71a11356dee49efe2e99
Hai Tran Dong <hai.trandong@efundamentals.com>
2023-10-03 23:29:59 +0700

    rename package

--- src/main/java/com/shop/user/ServletInitializer.java
+++ src/main/java/com/shop/user/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.digital.user;
+package com.shop.user;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
